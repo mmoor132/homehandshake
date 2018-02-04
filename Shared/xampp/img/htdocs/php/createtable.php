b55a21e42bc9872f72911e083b6282c8901a3c37
Mason Moore <mmoor132@kent.edu>
2018-02-04 18:13:48 -0500

    Mason Moore
    Made a few file path changes to make it friendlier to mobile testing

---
+++ Shared/xampp/img/htdocs/php/createtable.php
@@ -0,0 +1,20 @@
+<?php
+include 'db_connection.php';
+
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
