d343fa9fcab714f5968cf5e9aae4355b95d06d3b
Mason Moore <mmoor132@kent.edu>
2018-02-21 01:14:01 -0500

    Mason
    Minior changes to PHP file for Profile Page

--- php/login2.php
+++ php/login2.php
@@ -14,23 +14,30 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Assigns Sessions values back to local variables
 $username = $_SESSION["username"];
 $password = $_SESSION["password"];
 
+// Perpare connection
 $stmt = $conn->prepare("SELECT userid FROM users WHERE username = '$username' AND password = '$password'");
 
+// Execute Query
 $stmt->execute();
 
+// Assign Result
 $result = $stmt->get_result();
 
+// Cylce through resut and assignning Value
 while ($row = $result->fetch_assoc()){
 
 	echo $row["userid"];
 	$_SESSION["userid"] = $row["userid"];
 }
 
+// Assigns Sessions values back to local variables
 $userid = $_SESSION["userid"];
 
+// Query For Active Listing
 $stm = $conn->prepare("
 	SELECT listings.listingid, listings.price, listings.address, listings.city, listings.zip
 	FROM listings
@@ -38,10 +45,13 @@ $stm = $conn->prepare("
 	ON listings.userid = users.userid
 	WHERE listings.userid = '$userid'");
 
+// Execute Query
 $stm->execute();
 
+// Assign Result
 $result = $stm->get_result();
 
+// Cylce through resut and assignning Value
 while ($row = $result->fetch_assoc()){
 	echo "<br>";
   	echo $row["listingid"];
@@ -56,10 +66,8 @@ while ($row = $result->fetch_assoc()){
 	echo "<br>";
 }
 
-
-
-
-
+// Close connection
+$conn->close();
 
 ?>
 
